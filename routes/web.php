9d2195a37ec132858133198cb5ad9a50a406a55f
Willy Arisky <willy.arisky@gmail.com>
2019-03-20 21:34:50 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,19 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// routes for admin
+Route::middleware(['auth', 'roles:admin'])->group(function () {
+    Route::get('/admin', 'AdminController@index')->name('admin');
+});
+// routes for user
+Route::middleware(['auth', 'roles:user'])->group(function () {
+    Route::get('/user', 'UserController@index')->name('user');
+});
+// routes for both roles
+Route::middleware(['auth', 'roles:admin,user'])->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
