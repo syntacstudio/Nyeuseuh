06df74a123c71678a13ca7122e20c50f36242638
Willy Arisky <willy.arisky@gmail.com>
2019-03-20 21:36:23 +0700

    Add namespace auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,16 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect destination by User role
+     */
+    public function redirectTo()
+    {
+        if (Auth::user()->hasRole('admin')) {
+            return route('admin');
+        }
+        return route('user');
     }
 }
