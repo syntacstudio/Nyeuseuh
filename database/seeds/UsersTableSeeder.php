6b1a1b78991bb122654937b8b3d398244cddb1ac
Willy Arisky <willy.arisky@gmail.com>
2019-03-20 20:25:54 +0700

    Add seeder for User

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+        $role_user  = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = "Jhon Doe";
+        $admin->email = "admin@test.app";
+        $admin->password = bcrypt('secret');
+        $admin->save();
+        $admin->roles()->attach($role_admin);
+
+        $user = new User();
+        $user->name = "Gal Gadot";
+        $user->email = "user@test.app";
+        $user->password = bcrypt('secret');
+        $user->save();
+        $user->roles()->attach($role_user);
+
     }
 }
