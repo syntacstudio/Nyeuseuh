328cc4e1708fb9d4331fb6d812048eba90d256e2
Willy Arisky <willy.arisky@gmail.com>
2019-03-20 21:35:58 +0700

    Add method to view user panel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Show the application dashboard for admin.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('user');
+    }
 }
